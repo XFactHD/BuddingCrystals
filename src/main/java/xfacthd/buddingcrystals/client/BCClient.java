814689f06b4f35c069c3198025e9273360a76bbb
XFactHD <xfacthd@gmx.de>
2022-05-29 23:55:25 +0200

    Remove superseded fallback texture reader

--- src/main/java/xfacthd/buddingcrystals/client/BCClient.java
+++ src/main/java/xfacthd/buddingcrystals/client/BCClient.java
@@ -1,15 +1,12 @@
 package xfacthd.buddingcrystals.client;
 
-import io.github.lukebemish.dynamic_asset_generator.client.api.JsonReaderAPI;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import xfacthd.buddingcrystals.BuddingCrystals;
-import xfacthd.buddingcrystals.client.util.FallbackTextureReader;
 import xfacthd.buddingcrystals.common.BCContent;
 
 @Mod.EventBusSubscriber(modid = BuddingCrystals.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -20,15 +17,6 @@ public final class BCClient
     {
         BCContent.allClusters().forEach(block ->
                 ItemBlockRenderTypes.setRenderLayer(block, RenderType.cutout())
-        );
-    }
-
-    static
-    {
-        // This must happen very early
-        JsonReaderAPI.registerTexSourceReadingType(
-                new ResourceLocation(BuddingCrystals.MOD_ID, "fallback_texture"),
-                new FallbackTextureReader()
         );
     }
 
